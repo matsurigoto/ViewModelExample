9c2c07b1573250b48a2ba30816f8657aa3712289
Duran Hsieh <chhsie@microsoft.com>
2019-07-17 09:46:50 +0800

    initial

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.ViewModel;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            CreateStudentViewModel model = new CreateStudentViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Create(CreateStudentViewModel model)
+        {
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/ViewModel/CreateStudentViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.ViewModel
+{
+    public class CreateStudentViewModel
+    {
+        [DisplayName("學號")]
+        [Required]
+        public string ID { get; set; }
+
+        [DisplayName("姓名")]
+        public string NAME { get; set; }
+
+        [DisplayName("地址")]
+        public string ADDRESS { get; set; }
+
+        [DisplayName("電子郵件")]
+        [EmailAddress]
+        public string EMAIL { get; set; }
+
+        [DisplayName("電話")]
+        [RegularExpression(@"^[0-9]*$")]
+        public string TEL { get; set; }
+
+    }
+}
